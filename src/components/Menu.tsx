c752dbf5de0b0be4559f24bcc7d00095020a29b6
JC-Aguiar <jcostalaguiar@gmail.com>
2021-12-20 18:04:54 -0300

    Configurations and Style

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,100 +1,173 @@
 import {
-  IonContent,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
-  IonMenu,
-  IonMenuToggle,
-  IonNote,
-} from '@ionic/react';
+    IonContent,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonMenu,
+    IonMenuToggle,
+    IonNote,
+    IonAccordionGroup,
+    IonAccordion,
+} from "@ionic/react";
 
-import { useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
-import './Menu.css';
+import { useLocation } from "react-router-dom";
+import {
+    filmOutline,
+    filmSharp,
+    carSportOutline,
+    carSportSharp,
+    roseOutline,
+    roseSharp,
+    heartHalfOutline,
+    heartHalfSharp,
+    happyOutline,
+    happySharp,
+    bookOutline,
+    bookSharp,
+    rocketOutline,
+    rocketSharp,
+    timeOutline,
+    timeSharp,
+    personCircleOutline,
+    personCircleSharp,
+} from "ionicons/icons";
+import "./Menu.css";
+import React, { useRef, useState } from "react";
+import { endpoint } from "../TMDB";
 
 interface AppPage {
-  url: string;
-  iosIcon: string;
-  mdIcon: string;
-  title: string;
+    url: string;
+    iosIcon: string;
+    mdIcon: string;
+    title: string;
+}
+
+interface MovieSumary {
+    id: number;
+    name: string;
+    icon: string;
+    genre: number[];
 }
 
 const appPages: AppPage[] = [
-  {
-    title: 'Inbox',
-    url: '/page/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
-  },
-  {
-    title: 'Outbox',
-    url: '/page/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
-  },
-  {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
-  }
+    {
+        title: "Navegação Livre",
+        url: "/movies/all",
+        iosIcon: filmOutline,
+        mdIcon: filmSharp,
+    },
+    {
+        title: "Ação",
+        url: "/movies/action",
+        iosIcon: carSportOutline,
+        mdIcon: carSportSharp,
+    },
+    {
+        title: "Romance",
+        url: "/movies/romance",
+        iosIcon: roseOutline,
+        mdIcon: roseSharp,
+    },
+    {
+        title: "Drama",
+        url: "/movies/drama",
+        iosIcon: heartHalfOutline,
+        mdIcon: heartHalfSharp,
+    },
+    {
+        title: "Comédia",
+        url: "/movies/comedy",
+        iosIcon: happyOutline,
+        mdIcon: happySharp,
+    },
+    {
+        title: "Documentário",
+        url: "/movies/documentary",
+        iosIcon: bookOutline,
+        mdIcon: bookSharp,
+    },
+    {
+        title: "Fantasia",
+        url: "/movies/fantasy",
+        iosIcon: rocketOutline,
+        mdIcon: rocketSharp,
+    },
 ];
 
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+const userRecents: MovieSumary[] = [
+    {
+        id: 550,
+        name: "Fight Club",
+        icon: "Fight Club",
+        genre: [28],
+
+    }
+];
 
 const Menu: React.FC = () => {
-  const location = useLocation();
+    const location = useLocation();
 
-  return (
-    <IonMenu contentId="main" type="overlay">
-      <IonContent>
-        <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
-          {appPages.map((appPage, index) => {
-            return (
-              <IonMenuToggle key={index} autoHide={false}>
-                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
-                  <IonLabel>{appPage.title}</IonLabel>
+    const loadMovie = () => endpoint(28);
+
+    const iconColor = "#ffe365";
+
+    const accordionGroupRef = useRef(null);
+
+    return (
+        <IonMenu contentId="main" type="overlay" id="menu">
+            <IonContent >
+                <IonItem>
+                    <IonLabel>
+                        <IonListHeader className="userName">JC-AGUIAR</IonListHeader>
+                        <IonNote>jcostalaguiar@gmail.com</IonNote>
+                    </IonLabel>
+                    <IonIcon slot="start" md={personCircleOutline} ios={personCircleSharp} color="dark" className="userAvatar"/>
                 </IonItem>
-              </IonMenuToggle>
-            );
-          })}
-        </IonList>
-
-        <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
-            </IonItem>
-          ))}
-        </IonList>
-      </IonContent>
-    </IonMenu>
-  );
+                {appPages.map((appPage, index) => {
+                    return (
+                        <IonMenuToggle key={index} autoHide={false} className="menuItem">
+                            <IonItem
+                                className={location.pathname === appPage.url ? "selected" : ""}
+                                routerLink={appPage.url}
+                                routerDirection="none"
+                                lines="none"
+                                detail={false}
+                            >
+                                <IonIcon
+                                    slot="start"
+                                    ios={appPage.iosIcon}
+                                    md={appPage.mdIcon}
+                                    color="#"
+                                />
+                                <IonLabel>{appPage.title}</IonLabel>
+                            </IonItem>
+                        </IonMenuToggle>
+                    );
+                })}
+                <IonAccordionGroup>
+                    <IonAccordion value="recents" className="menuItem">
+                        <IonItem slot="header">
+                            <IonIcon slot="start" ios={timeSharp} md={timeOutline} color="#"/>
+                            <IonLabel>Recentes</IonLabel>
+                        </IonItem>
+                        <IonList slot="content">
+                            <ol>
+                                {userRecents.map((movieSumary, index) => {
+                                    return(
+                                        <IonItem key={index}>
+                                            <li><IonLabel>{userRecents[index].name}</IonLabel></li>
+                                        </IonItem>
+                                    );
+                                })}
+                            </ol>
+                        </IonList>
+                    </IonAccordion>
+                </IonAccordionGroup>
+            </IonContent>
+        </IonMenu>
+    );
 };
 
 export default Menu;
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,4 +1,5 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
@@ -24,7 +25,6 @@ const Page: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
       </IonContent>
     </IonPage>
   );
